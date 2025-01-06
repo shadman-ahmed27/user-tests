5991608cdacc0789a0cc09e125a8a57687c3edf9
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-05 21:03:10 -0600

    feat: created a test for rendering email and name of each user

--- src/tests/UserList.test.js
+++ src/tests/UserList.test.js
@@ -1,14 +1,22 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "../UserList";
 
-test("render one row per user", () => {
-  // render the component
+function renderComponent() {
   const users = [
     { name: "John Doe", email: "ayo@ayo.com" },
     { name: "Jane Doe", email: "aya@aya.com" },
   ];
 
   render(<UserList users={users} />);
+
+  return {
+    users,
+  };
+}
+
+test("render one row per user", () => {
+  // render the component
+  renderComponent();
 
   // find the rows in the table
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
@@ -17,4 +25,14 @@ test("render one row per user", () => {
   expect(rows).toHaveLength(2);
 });
 
-// test('renders email and name of each user', () => {});
+test("renders email and name of each user", () => {
+  const { users } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
